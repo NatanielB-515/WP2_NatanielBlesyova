5e153cced0da6f2407c454e4be0a2b1531361abf
nataniel <nataniel515@gmail.com>
2022-03-15 13:39:54 +0700

    Menambahkan Contoh1.php

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function Index()
+    {
+            echo"<h1>Perkenalkan </h1>";
+            echo"Nama Saya Nataniel Blesyova<br>
+            Saya tinggal di daerah Cibubur<br>
+            Olahraga yang saya sukai adalah Berenang<br>";
+    }
+
+}
