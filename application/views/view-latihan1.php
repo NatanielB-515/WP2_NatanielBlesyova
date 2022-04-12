089ded1c7532be4ec8aeb0bbdf054858dc1553eb
Nataniel Blesyova <nataniel515@gmail.com>
2022-04-12 08:16:00 +0700

    (Latihan MVC) Menambahkan Latihan1.php, Model_latihan1.php, view-latihan1.php

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Yuk kita belajar web programming..!!<br>
+    Nilai1 = <?= $nilai1; ?> <br>
+    Nilai2 = <?= $nilai2; ?> <br>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
